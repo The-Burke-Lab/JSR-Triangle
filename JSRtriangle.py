2beeb7c61601539e810fca42b528095ffa7ef329
rodger <rc3141@columbia.edu>
2018-08-24 13:07:59 -0400

    updating JSR triangle code

--- JSRtriangle.py
+++ JSRtriangle.py
@@ -1,69 +1,68 @@
-# -*- coding: utf-8 -*-
 """
-Created on Fri Aug 17 08:55:49 2018
-
+Created on Fri Aug 17 2018
 @author: RodgerCornell
 """
 import thermo as therm
 import matplotlib.pyplot as plt
 import numpy as np
-#------------------------------------------------------------------------------
+import pandas as pd
+
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 # JSR OPERATIONAL SPACE
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 #------------------------------------------------------------------------------
-Reactor = 0.020 # Test (Van Geem)    0.029 #[m]
-Nozzle = 900    # Test (Van Geem)    300    #[um]
+# Dimensions of the Reactor
+#------------------------------------------------------------------------------
+#Reactor = 0.029   # Burke Lab
+#Nozzle = 300      # Burke Lab 
+Reactor = 0.070    # Test (Matras R1)
+Nozzle = 1000      # Test (Matras R1)
 
 # Reactor Temperature [K]
-Temp =  900 # Test (Van Geem)      800
+Temp =  20 + 273.15
 # Residence Time [s]
-tau = 0.2
+tau = 5.0
 # Adimensional Constant
-A = np.pi/4 #0.3
+# *(we need to find more data on this constant. thers is supposedly a paper
+# that provides A as a function of temperature) 
+A = 0.3 #np.pi/4
 
 #------------------------------------------------------------------------------
-# SPECIES INFO
+# Species Information/Thermo Data
 #------------------------------------------------------------------------------
 # Stoichiometric Air/Methane
 #  (1)CH4 +  (2)(02 + 3.76N2) = (2)H2O + (1)CO2 + (7.52)N2
 #------------------------------------------------------------------------------
-C2H6 = therm.Chemical('ethane',T=Temp)                      # Test (Van Geem)
-H2O = therm.Chemical('water',T=Temp)                        # Test (Van Geem)
+N2 = therm.Chemical('n2',T=Temp)            # Test (Matras R1)
+O2 = therm.Chemical('o2',T=Temp)            # Test (Matras R1)
+Ar = therm.Chemical('argon',T=Temp)         # Test (Matras R1)
 
-y = ([0.769,0.231])                                         # Test (Van Geem)
-
-CH4 = therm.Chemical('methane',T=Temp)
-O2 = therm.Chemical('oxygen',T=Temp)
-N2 = therm.Chemical('nitrogen',T=Temp)
-
-# (CH4, O2, N2)
-## Mole Fractions []
-#n = np.array([0.095057,0.190114,0.714829])
+# (N2, O2, Ar)
+# Mole Fractions []
+n = np.array([0.7809,0.2095,0.0096])
 
 # Molecular Mass [g/mol]
-M = np.array([C2H6.MW,H2O.MW])                              # Test (Van Geem)
-#M = np.array([CH4.MW,O2.MW,N2.MW])
+M = np.array([N2.MW,O2.MW,Ar.MW])
 
-## Mass Fractions []
-#y = (n*M)/(sum(n*M))
-n = (y/M)/sum(y/M)                                          # Test (Van Geem)
+# Mass Fractions []
+# *(mole fraction below if given mass fraction) 
+y = (n*M)/(sum(n*M))
+#n = (y/M)/sum(y/M)
 
 # Universal Gas Constant [J/(K*mol)]
 R_uni = 8.3144598 
 
 # Cp [J/(K*g)]
-Cp = np.array([C2H6.Cpg/1000,H2O.Cpg/1000])                 # Test (Van Geem)
-#Cp = np.array([CH4.Cpg/1000,O2.Cpg/1000,N2.Cpg/1000])
+Cp = np.array([N2.Cpg/1000,O2.Cpg/1000,Ar.Cpg/1000])
 
 # Mass Density [g/m^3]
-rho = np.array([C2H6.rhog*1000,H2O.rhog*1000])              # Test (Van Geem) 
-#rho = np.array([CH4.rhog*1000,O2.rhog*1000,N2.rhog*1000]) 
+rho = np.array([N2.rhog*1000,O2.rhog*1000,Ar.rhog*1000]) 
 
 # Dynamic Viscosity [(g/(m*s^2))*s]
-nu = np.array([C2H6.mug*1000,H2O.mug*1000])                 # Test (Van Geem)
-#nu = np.array([CH4.mug*1000,O2.mug*1000,N2.mug*1000])
+nu = np.array([N2.mug*1000,O2.mug*1000,Ar.mug*1000])
 
 #------------------------------------------------------------------------------
-# MIXTURE INFO
+# Mixture Information/Thermo Data
 #------------------------------------------------------------------------------
 # Molecular Mass [g/mol]
 M_mix = sum(n*M)
@@ -77,6 +76,9 @@ nu_mix = sum(n*nu)
 k_mix = (sum(y*Cp))/((sum(y*Cp)-R_mix))
 c_mix = np.sqrt(k_mix*R_mix*Temp*1000)
 
+#------------------------------------------------------------------------------
+# The Three Limits
+#------------------------------------------------------------------------------
 # TURBULENCE
 # The jets flowing from the nozzles must be turbulent. This imposes an upper
 # limit of the residence time. Putting this in terms of Reactor Radius (R)
@@ -88,7 +90,7 @@ for x in range(1,Nozzle*1000,1):
     dt.append(x)
     Rt.append(((230*tau*nu_mix*x)/(rho_mix*A))**(1/3))
 
-# RECYCLING Rate
+# RECYCLING RATE
 # This is the ratio of residence time to the time it takes to carry all gas in 
 # the reactor from one nozzle to the adjacent one. Experimentally observed that
 # recycling rate must be >30 for good gas-phase mixing. This creates a lower
@@ -112,10 +114,12 @@ for x in range(1,Nozzle*1000,1):
     ds.append(x)    
     Rs.append(((3*tau*(x**2)*c_mix)/4)**(1/3))
     
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+# PLOTTING THE RESULTS
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 #------------------------------------------------------------------------------
-# PLOTTING RESULTS
+# Finding the Corners of the Triangle to Bound an Operational Space
 #------------------------------------------------------------------------------
-# Finding the ends of the triangle to bound the shaded region
 point1d = (920*nu_mix)/(3*rho_mix*A*c_mix)
 point1R = ((3*tau*(point1d**2)*c_mix)/4)**(1/3)
 
@@ -125,22 +129,27 @@ point2R = (60*point2d)/(np.pi*A)
 point3d = ((230*tau*nu_mix*(np.pi**3)*(A**2))/(rho_mix*(60**3)))**(1/2)
 point3R = ((230*tau*nu_mix*point3d)/(rho_mix*A))**(1/3)
 
-#plt.loglog(point1R,point1d,"o",color='y')
-#plt.loglog(point2R,point2d,"o",color='y')
-#plt.loglog(point3R,point3d,"o",color='y')
+#------------------------------------------------------------------------------
+# Setting Plot Boundaries 
+#------------------------------------------------------------------------------
+plt.xlim([point1R - 0.3*point1R,point2R + 0.3*point2R])                                    
+plt.ylim([point1d - 0.3*point1d,point2d + 0.3*point2d])                                
 
 #------------------------------------------------------------------------------
-plt.xlim([point1R - 0.2*point1R,point2R + 0.2*point2R])                                    
-plt.ylim([point1d - 0.2*point1d,point2d + 0.2*point2d])                                
+# Setting the aspect ratio of the plot
+#------------------------------------------------------------------------------
+plt.gca().set_aspect(aspect=0.5,adjustable='box')
 
 #------------------------------------------------------------------------------
-# Combining two lines into one to make shading possible with 'fill_between' 
+# Combining Turbulence and Recycling lines to make shading possible 
+#------------------------------------------------------------------------------
 line4 = np.maximum(Rt,Rr)
-# Cutting down the line
+# Bounding this new line using the triangle corners
 line4 = line4[np.logical_and(line4>point1R,line4<point2R)]
-#line4 = line4[np.logical_and(x>point1R,x<point2R)]
+
 #------------------------------------------------------------------------------
-# Using the triangle ends to limit the shading and locate the plot text
+# Shading the triangle and locating the plot text
+#------------------------------------------------------------------------------
 step=[]
 ds_cut=[]
 Rs_cut=[]
@@ -162,32 +171,52 @@ for x in range(1,Nozzle*1000,1):
     if point1d < x < point3d:
         dt_cut.append(dt[y])
         Rt_cut.append(Rt[y])
+plt.fill_betweenx(step,line4,Rs_cut,color='grey',alpha='0.2')     
 
-# Finding logarithmic means to plot labels
-Mean13x = (point3R-point1R)/(np.log(point3R)-np.log(point1R))
-Mean13y = (point3d-point1d)/(np.log(point3d)-np.log(point1d))  
-Mean32x = (point3R-point2R)/(np.log2(point3R)-np.log2(point2R))
-Mean32y = (point3d-point2d)/(np.log2(point3d)-np.log2(point2d))  
-Mean12x = (point2R-point1R)/(np.log(point2R)-np.log(point1R))
-Mean12y = (point2d-point1d)/(np.log2(point2d)-np.log2(point1d)) 
-     
+# Plotting the data on a log-log scale     
 plt.loglog(Reactor,Nozzle*10**(-6),"o",color='k')
-plt.loglog(Rt,dt,color='c', linestyle='--')
+plt.loglog(Rt,dt,color='b', linestyle='--')
 plt.loglog(Rr,dr,color='r', linestyle='-.')
 plt.loglog(Rs,ds,color='g', linestyle='-')
-plt.legend(['Reactor', 'Turbulence','Recycling','Sonic'],fancybox=True,loc='lower right')
-        
-plt.fill_betweenx(step,line4,Rs_cut,color='grey',alpha='0.2')      
+    
 #------------------------------------------------------------------------------
 plt.title('JSR Operational Limits', color='k')
-plt.ylabel('Nozzle Diameter (m)', color='k')
-plt.xlabel('Reactor Radius (m)', color='k')
+plt.ylabel('Nozzle Diameter [m]', color='k')
+plt.xlabel('Reactor Radius [m]', color='k')
+plt.legend(['Reactor','Turbulence Limit','Recycling Limit','Sonic Limit'],
+           fancybox=True,loc='lower right')
+# Adding tick marks to all sides of the plot 
+plt.tick_params(axis='both',which='both',direction='in',width=1.25,top=True,
+                right=True)
+plt.savefig('JSRplot.jpg',dpi=1200,bbox_inches='tight')
 
-plt.tick_params(axis='both',which='both',direction='in',width=1.25,top=True,right=True)
+#------------------------------------------------------------------------------
 
-#plt.text(Mean12x,Mean12y,'Sonic',color='g')
-#plt.text(Mean13x,Mean13y,'Turbulence',color='b')
-#plt.text(Mean32x,Mean32y,'Recycling',color='r')
 
-plt.savefig('whatever.jpg',dpi=1200,bbox_inches='tight')
-#------------------------------------------------------------------------------ 
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+# MATRAS VALIDATION
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+#------------------------------------------------------------------------------
+# Pulling in Digitized Matras Data for Validation
+#------------------------------------------------------------------------------
+#df1 = pd.read_excel("Matras_1s.xlsx")
+#plt.loglog(df1['Rt [m]'],df1['dt [m]'],'x',color='b')
+#plt.loglog(df1['Rr [m]'],df1['dr [m]'],'x',color='r')
+#plt.loglog(df1['Rs [m]'],df1['ds [m]'],'x',color='g')
+#plt.legend(['Turbulence','Recycling','Sonic','Matras_T','Matras_R','Matras_S']
+#           ,fancybox=True,loc='lower right')
+#plt.savefig('Matras_1s_validation.jpg',dpi=1200,bbox_inches='tight')
+
+#df5 = pd.read_excel("Matras_5s.xlsx")
+#plt.loglog(df5['Rt [m]'],df5['dt [m]'],'x',color='b')
+#plt.loglog(df5['Rr [m]'],df5['dr [m]'],'x',color='r')
+#plt.loglog(df5['Rs [m]'],df5['ds [m]'],'x',color='g')
+#plt.legend(['Turbulence','Recycling','Sonic','Matras_T','Matras_R','Matras_S']
+#           ,fancybox=True,loc='lower right')
+#plt.savefig('Matras_5s_validation.jpg',dpi=1200,bbox_inches='tight')
+
+
+
+
+
+ 
